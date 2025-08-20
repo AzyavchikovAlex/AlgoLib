050e8cce3f9c605c6f2281a765c3b7f31b83447a
AlexAzyavchikov <azyavchikov.alex@gmail.com>
2025-08-20 14:30:55 +0300

    add radix sort algorithm with almost no additional memory usage

--- RadixSort/benchmark.cpp
+++ RadixSort/benchmark.cpp
@@ -11,9 +11,9 @@ void MeasureRadix(benchmark::State& state) {
   std::uniform_int_distribution<int64_t> dist{0, std::numeric_limits<int64_t>::max()};
   std::generate(data.begin(), data.end(), [&]() { return dist(rng); });
 
-  for (auto s_ : state) {
+  for (auto _ : state) {
     auto v = data;
-    RadixSortAsc(v, [](int64_t x) {
+    RadixSortAsc(v.begin(), v.end(), [](int64_t x) {
       return x;
     });
     benchmark::ClobberMemory(); // Prevents the compiler from optimizing away the copy and sort.
@@ -31,19 +31,39 @@ void MeasureStd(benchmark::State& state) {
     // --- TIMED CODE ---
     // Create a copy to avoid timing the setup of already sorted data in subsequent iterations.
     auto v = data;
-    std::sort(v.begin(), v.end());
+    std::sort(v.begin(), v.end(), [](auto& x, auto& y) {
+      return x < y;
+    });
     benchmark::ClobberMemory(); // Prevents the compiler from optimizing away the copy and sort.
   }
 }
 
-BENCHMARK(MeasureRadix)->Arg(1'000);
-BENCHMARK(MeasureRadix)->Arg(10'000);
-BENCHMARK(MeasureRadix)->Arg(100'000);
-BENCHMARK(MeasureStd)->Arg(1000);
-BENCHMARK(MeasureStd)->Arg(10'000);
-BENCHMARK(MeasureStd)->Arg(100'000);
+BENCHMARK(MeasureRadix)->Arg(1 << 11);
+BENCHMARK(MeasureStd)->Arg(1 << 11);
+BENCHMARK(MeasureRadix)->Arg(1 << 12);
+BENCHMARK(MeasureStd)->Arg(1 << 12);
+BENCHMARK(MeasureRadix)->Arg(1 << 13);
+BENCHMARK(MeasureStd)->Arg(1 << 13);
+BENCHMARK(MeasureRadix)->Arg(1 << 14);
+BENCHMARK(MeasureStd)->Arg(1 << 14);
+BENCHMARK(MeasureRadix)->Arg(1 << 15);
+BENCHMARK(MeasureStd)->Arg(1 << 15);
+BENCHMARK(MeasureRadix)->Arg(1 << 16);
+BENCHMARK(MeasureStd)->Arg(1 << 16);
+BENCHMARK(MeasureRadix)->Arg(1 << 17);
+BENCHMARK(MeasureStd)->Arg(1 << 17);
+BENCHMARK(MeasureRadix)->Arg(1 << 18);
+BENCHMARK(MeasureStd)->Arg(1 << 18);
+BENCHMARK(MeasureRadix)->Arg(1 << 19);
+BENCHMARK(MeasureStd)->Arg(1 << 19);
+BENCHMARK(MeasureRadix)->Arg(1 << 20);
+BENCHMARK(MeasureStd)->Arg(1 << 20);
+BENCHMARK(MeasureRadix)->Arg(1 << 21);
+BENCHMARK(MeasureStd)->Arg(1 << 21);
+BENCHMARK(MeasureRadix)->Arg(1 << 22);
+BENCHMARK(MeasureStd)->Arg(1 << 22);
+BENCHMARK(MeasureRadix)->Arg(1 << 23);
+BENCHMARK(MeasureStd)->Arg(1 << 23);
+
 
 BENCHMARK_MAIN();
-// TEST(BEnc, t1) {
-//   ::benchmark::RunSpecifiedBenchmarks();
-// }
